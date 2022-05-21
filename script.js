4480c26cd20ed91ddc48748d3b6a3a8f74581f6a
Ricardo Halmoguera Garcia <halmoguerar@gmail.com>
2022-05-21 21:28:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const DisplayTime = ()=>{
     hours= date.getHours() <10? `0${date.getHours()}`: date.getHours() >12?`${date.getHours()-hoursModifier}`:`${date.getHours()}`
     minutes= date.getMinutes() <10? `0${date.getMinutes()}`:`${date.getMinutes()}`
     seconds= date.getSeconds() <10? `0${date.getSeconds()}`:`${date.getSeconds()}`
-    amPm = hours <10? "AM" : "PM"
+    amPm = date.getHours() <10? "AM" : "PM"
     hoursDisplay.innerText= `${hours}:${minutes}:${seconds}`
     amPmDisplay.innerText = amPm
     
